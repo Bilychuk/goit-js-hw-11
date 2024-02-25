4dd61b170f0d0ff26887f2e01b05913642f97c5c
Bilychuk <144622855+Bilychuk@users.noreply.github.com>
2024-02-25 14:01:39 +0000

    Deploying to gh-pages from @ Bilychuk/goit-js-hw-11@f2d8a713f85690ac3bdbe78c63f886cb11d2a79d 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,31 +1,31 @@
-import{i as p,S as I}from"./assets/vendor-7659544d.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function f(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=f(e);fetch(e.href,t)}})();const L="/goit-js-hw-11/assets/x-octagon-4f06a8ee.svg",o=document.querySelector(".form"),g=document.querySelector(".gallery");o.addEventListener("submit",A);function A(u){u.preventDefault();const r=o.elements.word.value.trim();if(r===""){p.error({title:"Error",message:"Search query cannot be empty"});return}const y=`https://pixabay.com/api/?key=42515164-6d66b652ac661a4b366139ff0&q=${r}&image_type=photo&orientation=horizontal&safesearch=${!0}`;fetch(y).then(s=>{if(!s.ok)throw new Error("Image error");return s.json()}).then(s=>{const l=s.hits;if(console.log(l),l.length===0)return g.innerHTML="",o.reset(),p.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fafafb",messageSize:"16px",backgroundColor:"#ef4040",theme:"dark",iconUrl:L,maxWidth:"432px"});{let E=function(i){return i.map(({webformatURL:c,largeImageURL:m,tags:h,likes:d,views:S,comments:$,downloads:x})=>`<li class="gallery-item">
-                            <a class="gallery-link" href="${m}">
+import{i as c,S as u}from"./assets/vendor-5b791d57.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const m="/goit-js-hw-11/assets/x-octagon-4f06a8ee.svg";function g(o){const a=`https://pixabay.com/api/?key=42515164-6d66b652ac661a4b366139ff0&q=${o}&image_type=photo&orientation=horizontal&safesearch=${!0}`;return fetch(a).then(n=>{if(!n.ok)throw new Error("Image error");return n.json()}).catch(n=>{c.error({message:"Error while fetching images from Pixabay!",title:"Error",titleColor:"#fff",messageColor:"#fafafb",messageSize:"16px",backgroundColor:"#ef4040",theme:"dark",iconUrl:m,maxWidth:"432px"})}).finally(()=>{f.reset()})}function d(o){return o.map(({webformatURL:r,largeImageURL:i,tags:s,likes:e,views:t,comments:a,downloads:n})=>`<li class="gallery-item">
+                            <a class="gallery-link" href="${i}">
                             <img
                             class="gallery-image"
-                            src="${c}"
-                            data-source="${m}"
-                            alt="${h}"
+                            src="${r}"
+                            data-source="${i}"
+                            alt="${s}"
                             />
                             </a>
                             <div class="image-info">
                                 <ul class="image-info-list">
                                     <li class="image-item">
                                         <h2 class="image-text">Likes</h2>
-                                        <p class="image-quantity">${d}</p>
+                                        <p class="image-quantity">${e}</p>
                                     </li>
                                     <li class="image-item">
                                         <h2 class="image-text">Views</h2>
-                                        <p class="image-quantity">${S}</p>
+                                        <p class="image-quantity">${t}</p>
                                     </li>
                                     <li class="image-item">
                                         <h2 class="image-text">Comments</h2>
-                                        <p class="image-quantity">${$}</p>
+                                        <p class="image-quantity">${a}</p>
                                     </li>
                                     <li class="image-item">
                                         <h2 class="image-text">Downloads</h2>
-                                        <p class="image-quantity">${x}</p>
+                                        <p class="image-quantity">${n}</p>
                                     </li>
                                 </ul>
                             </div>
-                        </li>`).join("")},b=function(i){i.preventDefault();const{className:c,dataset:m,alt:h}=i.target;if(c!=="gallery-image")return;new I(".gallery a",{captionsData:"alt"}).refresh()};g.innerHTML=E(l),g.addEventListener("click",b)}o.reset()}).catch(s=>{alert("Error while fetching images from Pixabay!")})}
+                        </li>`).join("")}const f=document.querySelector(".form"),l=document.querySelector(".gallery"),h=document.querySelector("span");f.addEventListener("submit",p);function p(o){o.preventDefault();const r=f.elements.word.value.trim();if(r===""){c.error({title:"Error",titleColor:"#fff",message:"Search query cannot be empty",messageColor:"#fafafb",messageSize:"16px",backgroundColor:"#ef4040",theme:"dark",iconUrl:m,maxWidth:"432px"});return}g(r).then(i=>{const s=i.hits;if(s.length===0)return l.innerHTML="",c.error({message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"#fafafb",messageSize:"16px",backgroundColor:"#ef4040",theme:"dark",iconUrl:m,maxWidth:"432px"});{let e=function(t){t.preventDefault();const{className:a}=t.target;if(a!=="gallery-image")return;new u(".gallery .gallery-link",{captionsData:"alt"}).refresh()};l.innerHTML=d(s),l.addEventListener("click",e)}})}l.addEventListener("load",y);function y(){return h.classList.add("loader")}
 //# sourceMappingURL=commonHelpers.js.map
